41df00ab8d75303c14c1afa10ce7d4e2bf471d52
MintyU <mintyu.dev@gmail.com>
2025-12-01 15:40:35 +0900

    Lecture 22

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -12,11 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
-
-import java.lang.reflect.Field;
-import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -53,18 +49,7 @@ public class PostController {
             Model model
     ) {
         if (bindingResult.hasErrors()) {
-            String errorFieldName = "title";
-            String errorMessage = bindingResult
-                    .getFieldErrors()
-                    .stream()
-                    .map(fieldError -> (fieldError.getField() + "-" + fieldError.getDefaultMessage()).split("-", 3))
-                    .map(field -> "<!--%s--><li data-error-field-name=\"%s\">%s</li>".formatted(field[1], field[0], field[2]))
-                    .sorted()
-                    .collect(Collectors.joining("\n"));
-
-            model.addAttribute("errorMessage", errorMessage);
-
-            return "post/post/write";
+           return "post/post/write";
         }
 
         Post post = postService.write(form.getTitle(), form.getContent());
